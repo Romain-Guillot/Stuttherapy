3c0debd5a200868677a5787426c42ee52f5ca0c9
develob <rguillot42@gmail.com>
2019-07-26 16:02:41 +0800

    icon

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# stutterapy
+# Stuttherapy
 
-A new Flutter project.
 
-## Getting Started
+## Architecture
 
-This project is a starting point for a Flutter application.
+## Technologies used
 
-A few resources to get you started if this is your first Flutter project:
+## Database
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+### Resource
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- [Firebase plans](https://firebase.google.com/pricing?authuser=0)
+- [Add Firebase to your Flutter app](https://firebase.google.com/docs/flutter/setup)
+
+### Annexe
+
+*Firestore prices (July 27, 2019) :*
+
+![](doc_res/prices.png)
+*First column : Free plan, Second column : 25$/Month, Third column : Pay as you go*
+
+## Signed app
+
+You can find a copy of the keystore in the following file : `./key.jks.save` (not available on the git repo)
--- lib/exercise_library/test/exercises_test.dart
+++ lib/exercise_library/test/exercises_test.dart
@@ -1,13 +1,35 @@
+import 'package:stuttherapy/exercise_library/exercises.dart';
+import 'package:stuttherapy/exercise_library/settings.dart';
 import 'package:test/test.dart';
 
 void main() {
   group("tests for [ExerciseTheme] class", () {
-    test("description", () {
+    test("exercice theme initialization", () {
+      var theme = ExerciseTheme(name: "test", shortDescription: "short", longDescription: "long");
+      expect(theme.name, equals("test"));
+      expect(theme.shortDescription, equals("short"));
+      expect(theme.longDescription, "long");
+      expect(theme.settings, isNotNull);
+      expect(theme.settings.get(ExerciseTheme.SETTINGS_MANUALLY_CHECK), isNotNull);
+      expect(theme.settings.get(ExerciseTheme.SETTINGS_PERCEPTION), isNotNull);
+      expect(theme.settings.get(ExerciseTheme.SETTINGS_RECORD), isNotNull);
+      expect(theme.settings.get(ExerciseTheme.SETTINGS_RESOURCE), isNotNull);
       
+      var themeBis = ExerciseTheme(name: "test", shortDescription: "short", longDescription: "long", exercisesSettings: {"test_set": BooleanField(label: "ok")});
+      expect(themeBis.settings, isNotNull);
+      expect(themeBis.settings.get("test_set"), isNotNull);
+      expect(themeBis.settings.get("test_set"), TypeMatcher<BooleanField>());
+
+    });
+  });
+
+  group("tests for [Exercise] class", () {
+    test("exercise initialization", () {
+
     });
 
-    test("", () {
-
+    test("exercise resource", () {
+      
     });
   });
   
